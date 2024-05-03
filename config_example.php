836eec6aaca1f4ea05b5961dde356af77319ec47
Dimas Febriyanto <dimassfeb@gmail.com>
2024-05-03 07:07:58 +0700

    config_example.php

---
+++ config_example.php
@@ -0,0 +1,12 @@
+<?php
+
+$host = "localhost";
+$username = "";
+$password = "";
+$database = "hospital";
+
+$conn = mysqli_connect($host, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+}
