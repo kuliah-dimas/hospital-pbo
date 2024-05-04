54b8d50e93848318f2adf3713db66818c6ec7330
Evandri Ridho Hasmono <evandriridho321@gmail.com>
2024-05-04 22:04:39 +0700

    Update Hospital_add

--- admin/hospital_add.php
+++ admin/hospital_add.php
@@ -54,43 +54,93 @@ if (isset($_POST['submit'])) {
     <?php include('header_admin.php'); ?>
 
 
-    <div class="section_form_input">
-        <form class="form_custom" method="post">
+    <div class="flex h-screen justify-center items-center">
+      <form
+        class="flex flex-col items-center gap-5 w-3/4 sm:w-1/2 lg:w-1/4 h-auto p-10 bg-white rounded-lg border-2"
+        method="post"
+      >
+        <h2 class="text-4xl font-bold">Tambah Rumah Sakit</h2>
 
-            <h2>Tambah Rumah Sakit</h2>
+        <div class="flex flex-col gap-2 w-full">
+          <label for="fullName" class="font-bold">Name</label>
+          <input
+            class="border h-10 px-3 rounded-md"
+            type="text"
+            id="fullName"
+            name="fullName"
+            placeholder="Masukkan nama"
+            required
+          />
+        </div>
 
-            <div>
-                <label for="name">Name</label>
-                <input type="text" id="name" name="name" required>
-            </div>
+        <div class="flex flex-col gap-2 w-full">
+          <label for="alamat" class="font-bold">Alamat</label>
+          <input
+            class="border h-10 px-3 rounded-md"
+            type="text"
+            id="alamat"
+            name="alamat"
+            placeholder="Masukkan alamat"
+            required
+          />
+        </div>
 
-            <div>
-                <label for="address">Address</label>
-                <input type="text" id="address" name="address" required>
-            </div>
+        <div class="flex flex-col gap-2 w-full">
+          <label for="phone" class="font-bold">Nomor Telepon</label>
+          <input
+            class="border h-10 px-3 rounded-md"
+            type="text"
+            id="phone"
+            name="phone"
+            placeholder="Masukkan nomor telepon"
+            required
+          />
+        </div>
 
-            <div>
-                <label for="phone">Phone</label>
-                <input type="tel" id="phone" name="phone" required>
-            </div>
+        <div class="flex flex-col gap-2 w-full">
+          <label for="email" class="font-bold">Email</label>
+          <input
+            class="border h-10 px-3 rounded-md"
+            type="text"
+            id="email"
+            name="email"
+            placeholder="Masukkan email"
+            required
+          />
+        </div>
 
-            <div>
-                <label for="email">Email</label>
-                <input type="email" id="email" name="email" required>
-            </div>
+        <div class="flex flex-col gap-2 w-full">
+          <label for="website" class="font-bold">Website</label>
+          <input
+            class="border h-10 px-3 rounded-md"
+            type="text"
+            id="website"
+            name="website"
+            placeholder="Masukkan website"
+            required
+          />
+        </div>
 
-            <div>
-                <label for="website">Website</label>
-                <input type="text" id="website" name="website" required>
-            </div>
+        <div class="flex flex-col gap-2 w-full">
+          <label for="keterangan" class="font-bold">Keterangan</label>
+          <textarea
+            class="border h-36 px-3 rounded-md"
+            type="text"
+            name="keterangan"
+            id="keterangan"
+            placeholder="Masukan keterangan"
+          ></textarea>
+        </div>
 
-            <div>
-                <label for="description">Description</label>
-                <textarea id="description" name="description" required></textarea>
-            </div>
-
-            <button type="submit" name="submit" class="button_custom" value="submit">Submit</button>
-        </form>
+        <button
+          class="flex justify-center items-center font-bold text-lg text-white bg-black rounded-full w-full h-10 mt-5"
+          type="submit"
+          name="submit"
+          value="submit"
+        >
+          Submit
+        </button>
+      </form>
     </div>
 
     <?php include('../footer.php.php'); ?>
