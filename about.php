9b9779e7107736f41a08857fc1085c917bdd8938
Evandri Ridho Hasmono <evandriridho321@gmail.com>
2024-05-08 11:49:50 +0700

    Update About.php

---
+++ about.php
@@ -0,0 +1,212 @@
+<?php include('header_admin.php'); ?>
+<div class="flex flex-wrap ml-3 pt-28">
+      <h1 class="text-right text-5xl my-1">Tentang</h1>
+      <img src="./img/brand_logo.svg" alt="hospital-brand" class="mx-2" />
+      <h1 class="font-bold text-5xl my-2">HOSPITAL</h1>
+    </div>
+
+    <div class="container pt-10">
+      <div class="flex flex-wrap">
+        <div class="w-full self-center px-4 lg:w-1/2">
+          <p class="text-justify">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe,
+            veritatis? Odit autem repudiandae nisi rerum ab possimus alias
+            fugiat reprehenderit! Inventore, hic harum quos quae earum iste
+            ipsum eligendi voluptates? Odio nesciunt dolorum maxime, molestias
+            earum provident vel maiores consectetur, itaque similique minima,
+            sunt voluptatum accusantium ullam a doloremque error officia
+            voluptas eaque neque quibusdam. Assumenda sunt veniam delectus
+            harum. Non, dolorem id provident, minima hic culpa perspiciatis
+            dolore possimus recusandae aliquam iure? Ad culpa atque libero
+            illum, maxime impedit, necessitatibus quaerat blanditiis rem fuga
+            inventore eligendi, nobis commodi? Corrupti! Impedit nulla itaque
+            quo ipsum delectus, eveniet totam et repellat vero, perspiciatis
+            beatae velit nobis cum. Facere harum voluptatum laudantium omnis
+            exercitationem, quis minus dolorum tenetur, at voluptas molestias
+            reprehenderit! Iste delectus quidem cumque neque sed ipsa veniam!
+            Atque consectetur unde excepturi, adipisci quae qui omnis voluptates
+            tempore deserunt placeat beatae accusantium nulla laborum sed iure
+            molestiae. Libero, molestiae. Dicta. Earum quisquam ea, eum
+            architecto, vitae aliquam exercitationem eos nobis rerum officia
+            adipisci in, explicabo delectus similique. Eveniet distinctio facere
+            voluptas! Doloremque dignissimos, aliquid qui iste fuga saepe sit
+            animi! Similique vel magnam tenetur debitis soluta? Expedita optio
+            dicta distinctio totam enim laboriosam aperiam. Cupiditate veniam
+            neque illum ratione quis libero non repudiandae deleniti odio, aut
+            repellendus perferendis aliquid sapiente. Consectetur adipisci
+            itaque eaque eum reiciendis, voluptatibus excepturi eius, in magnam
+            labore, ipsum voluptates similique placeat dolores tempore nisi
+            laudantium totam cumque minus illo. Velit blanditiis aspernatur
+            tempore repellendus impedit. Eaque qui, natus alias deleniti dolor
+            voluptatem fugit excepturi magnam iure assumenda. Laborum, totam
+            officia fugit odit quis tempore accusantium, sit eveniet voluptates
+            velit at quibusdam delectus, amet consequuntur officiis. Quis porro
+            quam laudantium, laboriosam consequuntur quasi iure cupiditate sunt
+            voluptas labore dolorem necessitatibus perferendis autem
+            voluptatibus sequi unde blanditiis placeat esse ratione, quia aut
+            exercitationem officiis hic! Iure, obcaecati.
+          </p>
+        </div>
+        <div class="w-full self-center px-4 lg:w-1/2">
+          <img src="./img/search hospital.png" alt="search-hospital" />
+        </div>
+      </div>
+    </div>
+
+    <div class="container pt-28">
+      <div class="flex flex-wrap">
+        <div class="w-full md:w-1/2 mb-4">
+          <div
+            class="mx-4 self-center h-96 bg-gray-100 rounded-lg overflow-hidden shadow-sm"
+          >
+            <div class="w-full flex items-center justify-center">
+              <img src="./img/twemoji_eye.png" alt="eye-emoji" />
+            </div>
+            <div class="px-6 py-4">
+              <div class="font-bold text-xl mb-2 text-center">Visi Kami</div>
+              <p class="text-slate-700 text-base">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic
+                iste eius amet ad sed cum maiores eligendi earum ullam sit
+                blanditiis quibusdam deleniti voluptatum, aliquid nemo suscipit!
+                Officia, totam voluptas.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="w-full md:w-1/2 mb-4">
+          <div
+            class="mx-4 self-center h-96 bg-gray-100 rounded-lg overflow-hidden shadow-sm"
+          >
+            <div class="w-full flex items-center justify-center">
+              <img src="./img/octicon_goal-16.png" alt="goal-emoji" />
+            </div>
+            <div class="px-6 py-4">
+              <div class="font-bold text-xl mb-2 text-center">Misi Kami</div>
+              <p class="text-slate-700 text-base">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Repellat dolorum deleniti similique impedit reprehenderit dolor
+                modi cupiditate facilis aperiam incidunt. Quidem sit laborum ut
+                vitae quasi molestias numquam distinctio nam.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container pt-28">
+      <h2 class="text-center text-4xl font-semibold">Apa yang kami lakukan</h2>
+      <div class="container pt-10">
+        <div class="flex flex-wrap">
+          <div
+            class="w-full h-96 mx-4 bg-gray-100 flex justify-center items-center rounded-lg shadow-sm lg:w-1/3 lg:ml-36 lg:h-100"
+          >
+            <p class="text-cente text-bold text-4xl">Foto</p>
+          </div>
+
+          <div class="w-full self-center px-4 lg:w-1/2 lg:ml-10">
+            <h1 class="font-semibold text-3xl text-{#294282} pt-4">01</h1>
+            <h2 class="font-semibold text-3xl pt-4">hmmmmm</h2>
+            <p class="pt-4">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Dignissimos nihil aut modi dicta labore, quia veritatis adipisci
+              eaque qui? Ullam illo quisquam impedit quia, assumenda repellendus
+              nobis id et ipsa unde exercitationem consequuntur aut eveniet ad
+              dolor? Eveniet iste exercitationem consectetur harum quibusdam
+              modi alias error! Voluptatum sed ullam magni earum fuga nostrum
+              nesciunt! Reiciendis ab modi harum sed optio tenetur quae eveniet
+              provident voluptatibus vel, adipisci fugiat deserunt soluta eos
+              beatae dolor voluptatum vero possimus. Temporibus labore a
+              dignissimos eius, omnis magni numquam reiciendis suscipit? Quidem
+              totam ea neque aspernatur unde nemo porro deserunt alias cumque.
+              In, mollitia harum.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container pt-10">
+      <div class="flex flex-wrap">
+        <div class="w-full self-center px-4 lg:w-1/2 lg:ml-10">
+          <h1 class="font-semibold text-3xl text-{#294282} pt-4">02</h1>
+          <h2 class="font-semibold text-3xl pt-4">hmmmmm</h2>
+          <p class="py-4">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Dignissimos nihil aut modi dicta labore, quia veritatis adipisci
+            eaque qui? Ullam illo quisquam impedit quia, assumenda repellendus
+            nobis id et ipsa unde exercitationem consequuntur aut eveniet ad
+            dolor? Eveniet iste exercitationem consectetur harum quibusdam modi
+            alias error! Voluptatum sed ullam magni earum fuga nostrum nesciunt!
+            Reiciendis ab modi harum sed optio tenetur quae eveniet provident
+            voluptatibus vel, adipisci fugiat deserunt soluta eos beatae dolor
+            voluptatum vero possimus. Temporibus labore a dignissimos eius,
+            omnis magni numquam reiciendis suscipit? Quidem totam ea neque
+            aspernatur unde nemo porro deserunt alias cumque. In, mollitia
+            harum.
+          </p>
+        </div>
+
+        <div
+          class="w-full h-96 mx-4 bg-gray-100 flex justify-center items-center rounded-lg shadow-sm lg:w-1/3 lg:mr-36 lg:h-100"
+        >
+          <p class="text-center text-bold text-4xl">Foto</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="flex justify-center items-center pt-28 mb-10 w-full px-5">
+      <form
+        class="flex flex-col items-center gap-5 sm:w-1/2 h-auto p-10 bg-white rounded-lg border-2"
+        method="post"
+      >
+        <h2 class="text-4xl sm:text-2xl font-bold">Contact Us</h2>
+
+        <div class="flex flex-col gap-2 w-full">
+          <label for="name" class="font-bold">Nama Lengkap</label>
+          <input
+            class="border h-10 p-3 rounded-md"
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Masukkan nama anda"
+            required
+          />
+        </div>
+
+        <div class="flex flex-col gap-2 w-full">
+          <label for="email" class="font-bold">Email</label>
+          <input
+            class="border h-10 p-3 rounded-md"
+            type="text"
+            id="email"
+            name="email"
+            placeholder="Masukkan email anda"
+            required
+          />
+        </div>
+
+        <div class="flex flex-col gap-2 w-full">
+          <label for="description" class="font-bold">Pesan</label>
+          <textarea
+            class="border h-36 p-3 rounded-md"
+            type="text"
+            name="pesan"
+            id="pesan"
+            placeholder="pesan"
+          ></textarea>
+        </div>
+
+        <button
+          class="flex justify-center items-center font-bold text-lg text-white bg-black rounded-full w-full h-10 mt-5"
+          type="submit"
+          name="submit"
+          value="submit"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+
+<?php include('../footer.php.php'); ?>
