54826039f38712f0b8b4dd70116439588a16cbfa
Dimas Febriyanto <dimassfeb@gmail.com>
2024-05-09 11:39:09 +0700

    update: fix detail_hospital and hospital_list

--- detail_hospital.php
+++ detail_hospital.php
@@ -81,7 +81,7 @@ if (isset($_POST['submitEdit'])) {
             </div>
             <div>
                 <?php if (isset($image)) : ?>
-                    <img class="h-[385px] w-[663px] rounded-lg" src="<?= $image ?>" alt="">
+                <img class="h-[385px] w-[663px] rounded-lg" src="<?= $image ?>" alt="">
                 <?php endif; ?>
             </div>
             <div class="text-justify ">
@@ -114,7 +114,9 @@ if (isset($_POST['submitEdit'])) {
                     <div class="bg-[#9747ff] w-max p-3 font-bold text-white rounded-t-lg">Daftar Dokter
                     </div>
                     <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" clip-rule="evenodd" d="M0 32H32C20.7989 32 15.1984 32 10.9202 29.8201C7.15695 27.9027 4.09734 24.8431 2.17987 21.0798C0 16.8016 0 11.201 0 0V32Z" fill="#9747FF" />
+                        <path fill-rule="evenodd" clip-rule="evenodd"
+                            d="M0 32H32C20.7989 32 15.1984 32 10.9202 29.8201C7.15695 27.9027 4.09734 24.8431 2.17987 21.0798C0 16.8016 0 11.201 0 0V32Z"
+                            fill="#9747FF" />
                     </svg>
                 </div>
                 <div class="overflow-x-auto w-full">
@@ -132,12 +134,12 @@ if (isset($_POST['submitEdit'])) {
                             $count = 1;
                             while ($row = $resultDoctorAtHospital->fetch_assoc()) :
                             ?>
-                                <tr class="text-center ">
-                                    <td class="border-r border-r-2 p-3"><?= $count++ ?></td>
-                                    <td class="border-r border-r-2 font-bold p-3"><?= $row['doctor_name']; ?></td>
-                                    <td class="border-r border-r-2 p-3"><?= $row['specialization']; ?></td>
-                                    <td class="border-r border-r-2 p-3"><?= $row["phone"] ?></td>
-                                </tr>
+                            <tr class="text-center ">
+                                <td class="border-r border-r-2 p-3"><?= $count++ ?></td>
+                                <td class="border-r border-r-2 font-bold p-3"><?= $row['doctor_name']; ?></td>
+                                <td class="border-r border-r-2 p-3"><?= $row['specialization']; ?></td>
+                                <td class="border-r border-r-2 p-3"><?= $row["phone"] ?></td>
+                            </tr>
                             <?php endwhile; ?>
                         </tbody>
                     </table>
@@ -149,83 +151,100 @@ if (isset($_POST['submitEdit'])) {
                 <?php
                 while ($row = $resultGetRating->fetch_assoc()) :
                 ?>
-                    <div class=" bg-[#F5F5F5]/50 p-4 mt-5 rounded-md border-2">
-                        <div class="flex items-center gap-4">
-                            <div class="flex">
-                                <div class="flex justify-center items-center text-white w-8 h-8 sm:w-[55px] sm:h-[55px] bg-black rounded-full">
-                                    <i class="fa fa-user"></i>
-                                </div>
-                            </div>
-                            <div class="text-xs sm:text-sm">
-                                <span class="font-bold"><?= $row['full_name']; ?></span>
-                                <div class="flex items-center gap-2">
-                                    <div class="rating flex  gap-2 items-center px-3 py-1 bg-[#DCE4FA] rounded-full">
-                                        <i class="fa fa-star text-yellow-500"></i>
-                                        <div class="font-bold"><?= $row['rating_value'] ?></div>
-                                    </div>
-                                    <span><?= $row['created_at'] ?></span>
-                                </div>
+                <div class=" bg-[#F5F5F5]/50 p-4 mt-5 rounded-md border-2">
+                    <div class="flex items-center gap-4">
+                        <div class="flex">
+                            <div
+                                class="flex justify-center items-center text-white w-8 h-8 sm:w-[55px] sm:h-[55px] bg-black rounded-full">
+                                <i class="fa fa-user"></i>
                             </div>
                         </div>
-                        <div class="mt-5">
-                            <?= $row['comment']; ?>
+                        <div class="text-xs sm:text-sm">
+                            <span class="font-bold"><?= $row['full_name']; ?></span>
+                            <div class="flex items-center gap-2">
+                                <div class="rating flex  gap-2 items-center px-3 py-1 bg-[#DCE4FA] rounded-full">
+                                    <i class="fa fa-star text-yellow-500"></i>
+                                    <div class="font-bold"><?= $row['rating_value'] ?></div>
+                                </div>
+                                <span><?= $row['created_at'] ?></span>
+                            </div>
                         </div>
                     </div>
+                    <div class="mt-5">
+                        <?= $row['comment']; ?>
+                    </div>
+                </div>
                 <?php endwhile; ?>
 
                 <div class="mt-10">
                     <form class="rating_card " method="post">
                         <?php if ($isAuthenticated) : ?>
-                            <?php if (mysqli_num_rows($resultGetRating) == 0) : ?>
-                                <div class="mb-5">
-                                    Jadi dirimu yang pertama memberikan rating.
-                                </div>
-                            <?php endif; ?>
-                            <?php if (mysqli_num_rows($resultUserRating) != 0) : ?>
-                                <div class="font-bold">Edit Rating</div>
-                                <div class="flex items-center mt-3">
-                                    <?php for ($i = 1; $i <= 5; $i++) : ?>
-                                        <input type="radio" id="star<?= $i ?>" name="rate" value="<?= $i ?>" <?= $i == $userRating ? 'checked' : '' ?> class="hidden" disabled />
-                                        <label for="star<?= $i ?>" title="text" class="cursor-pointer" onclick="handleRating(<?= $i ?>)">
-                                            <svg class="w-6 h-6 fill-current <?= $i <= $userRating ? 'text-yellow-500' : 'text-gray-500' ?>" viewBox="0 0 24 24">
-                                                <path d="M12 2l3.09 6.31 6.91.82-5 4.87 1.18 7.19L12 18.77l-6.09 3.22 1.18-7.19-5-4.87 6.91-.82L12 2z">
-                                                </path>
-                                            </svg>
-                                        </label>
-                                    <?php endfor; ?>
-                                </div>
+                        <?php if (mysqli_num_rows($resultGetRating) == 0) : ?>
+                        <div class="mb-5">
+                            Jadi dirimu yang pertama memberikan rating.
+                        </div>
+                        <?php endif; ?>
+                        <?php if (mysqli_num_rows($resultUserRating) != 0) : ?>
+                        <div class="font-bold">Edit Rating</div>
+                        <div class="flex items-center mt-3">
+                            <?php for ($i = 1; $i <= 5; $i++) : ?>
+                            <input type="radio" id="star<?= $i ?>" name="rate" value="<?= $i ?>"
+                                <?= $i == $userRating ? 'checked' : '' ?> class="hidden" disabled />
+                            <label for="star<?= $i ?>" title="text" class="cursor-pointer"
+                                onclick="handleRating(<?= $i ?>)">
+                                <svg class="w-6 h-6 fill-current <?= $i <= $userRating ? 'text-yellow-500' : 'text-gray-500' ?>"
+                                    viewBox="0 0 24 24">
+                                    <path
+                                        d="M12 2l3.09 6.31 6.91.82-5 4.87 1.18 7.19L12 18.77l-6.09 3.22 1.18-7.19-5-4.87 6.91-.82L12 2z">
+                                    </path>
+                                </svg>
+                            </label>
+                            <?php endfor; ?>
+                        </div>
 
-                                <textarea id="comment" name="comment" rows="4" cols="30" class="w-full mt-4 border rounded-lg focus:outline-none focus:border-blue-500 px-3 py-2" placeholder="Edit pesan" disabled><?= $userComment ?></textarea>
+                        <textarea id="comment" name="comment" rows="4" cols="30"
+                            class="w-full mt-4 border rounded-lg focus:outline-none focus:border-blue-500 px-3 py-2"
+                            placeholder="Edit pesan" disabled><?= $userComment ?></textarea>
 
-                                <button type="button" name="edit" value="edit" class="mt-4 px-6 py-2 bg-black text-white font-bold rounded-lg focus:outline-none" onclick="handleEdit()">Edit</button>
+                        <button type="button" name="edit" value="edit"
+                            class="mt-4 px-6 py-2 bg-black text-white font-bold rounded-lg focus:outline-none"
+                            onclick="handleEdit()">Edit</button>
 
-                                <button type="submit" name="submitEdit" value="submitEdit" class="hidden mt-4 px-6 py-2 bg-black text-white font-bold rounded-lg focus:outline-none">Submit</button>
+                        <button type="submit" name="submitEdit" value="submitEdit"
+                            class="hidden mt-4 px-6 py-2 bg-black text-white font-bold rounded-lg focus:outline-none">Submit</button>
 
-                                <button type="button" name="cancel" value="cancel" class="hidden mt-4 px-6 py-2 bg-black text-white font-bold rounded-lg focus:outline-none" onclick="handleCancel()">Cancel</button>
+                        <button type="button" name="cancel" value="cancel"
+                            class="hidden mt-4 px-6 py-2 bg-black text-white font-bold rounded-lg focus:outline-none"
+                            onclick="handleCancel()">Cancel</button>
 
-                            <?php else : ?>
-                                <div class="font-bold">
-                                    Berikan rating..
-                                </div>
-                                <div class="flex items-center ">
-                                    <?php for ($i = 1; $i <= 5; $i++) : ?>
-                                        <input type="radio" id="star<?= $i ?>" name="rate" value="<?= $i ?>" class="hidden" />
-                                        <label for="star<?= $i ?>" title="text" class="cursor-pointer" onclick="handleRating(<?= $i ?>)">
-                                            <svg class="w-6 h-6 fill-current text-gray-500" viewBox="0 0 24 24">
-                                                <path d="M12 2l3.09 6.31 6.91.82-5 4.87 1.18 7.19L12 18.77l-6.09 3.22 1.18-7.19-5-4.87 6.91-.82L12 2z">
-                                                </path>
-                                            </svg>
-                                        </label>
-                                    <?php endfor; ?>
-                                </div>
-                                <textarea id="comment" name="comment" rows="4" cols="30" class="w-full mt-4 border rounded-lg focus:outline-none focus:border-blue-500 px-3 py-2" placeholder="Tambahkan pesan"></textarea>
-
-                                <button type="submit" name="submit" value="submit" class="mt-4 px-6 py-2 bg-black text-white font-bold rounded-lg focus:outline-none">Submit</button>
-                            <?php endif; ?>
                         <?php else : ?>
-                            <div class="mt-4 text-sm"><a class="text-[#F56767]" href="login.php">Login</a>
-                                untuk memberikan rating.
-                            </div>
+                        <div class="font-bold">
+                            Berikan rating..
+                        </div>
+                        <div class="flex items-center ">
+                            <?php for ($i = 1; $i <= 5; $i++) : ?>
+                            <input type="radio" id="star<?= $i ?>" name="rate" value="<?= $i ?>" class="hidden" />
+                            <label for="star<?= $i ?>" title="text" class="cursor-pointer"
+                                onclick="handleRating(<?= $i ?>)">
+                                <svg class="w-6 h-6 fill-current text-gray-500" viewBox="0 0 24 24">
+                                    <path
+                                        d="M12 2l3.09 6.31 6.91.82-5 4.87 1.18 7.19L12 18.77l-6.09 3.22 1.18-7.19-5-4.87 6.91-.82L12 2z">
+                                    </path>
+                                </svg>
+                            </label>
+                            <?php endfor; ?>
+                        </div>
+                        <textarea id="comment" name="comment" rows="4" cols="30"
+                            class="w-full mt-4 border rounded-lg focus:outline-none focus:border-blue-500 px-3 py-2"
+                            placeholder="Tambahkan pesan"></textarea>
+
+                        <button type="submit" name="submit" value="submit"
+                            class="mt-4 px-6 py-2 bg-black text-white font-bold rounded-lg focus:outline-none">Submit</button>
+                        <?php endif; ?>
+                        <?php else : ?>
+                        <div class="mt-4 text-sm"><a class="text-[#F56767]" href="login.php">Login</a>
+                            untuk memberikan rating.
+                        </div>
                         <?php endif ?>
                     </form>
                 </div>
@@ -236,51 +255,51 @@ if (isset($_POST['submitEdit'])) {
 </div>
 
 <script>
-    function handleRating(rating) {
-        const radios = document.querySelectorAll('input[name="rate"]');
-        radios.forEach((radio, index) => {
+function handleRating(rating) {
+    const radios = document.querySelectorAll('input[name="rate"]');
+    radios.forEach((radio, index) => {
 
-            if (radio.disabled == true) {
-                return;
-            }
+        if (radio.disabled == true) {
+            return;
+        }
 
 
-            const starSVG = radio.nextElementSibling.querySelector('.w-6.h-6.fill-current');
-            if (index < rating) {
-                starSVG.classList.add('text-yellow-500');
-                starSVG.classList.remove('text-gray-500');
-            } else {
-                starSVG.classList.remove('text-yellow-500');
-                starSVG.classList.add('text-gray-500');
-            }
-        });
-    }
+        const starSVG = radio.nextElementSibling.querySelector('.w-6.h-6.fill-current');
+        if (index < rating) {
+            starSVG.classList.add('text-yellow-500');
+            starSVG.classList.remove('text-gray-500');
+        } else {
+            starSVG.classList.remove('text-yellow-500');
+            starSVG.classList.add('text-gray-500');
+        }
+    });
+}
 
-    function handleEdit() {
-        const editBtn = document.querySelector('[name="edit"]');
-        const cancelBtn = document.querySelector('[name="cancel"]');
-        const submitEditBtn = document.querySelector('[name="submitEdit"]');
-        const textarea = document.getElementById('comment');
+function handleEdit() {
+    const editBtn = document.querySelector('[name="edit"]');
+    const cancelBtn = document.querySelector('[name="cancel"]');
+    const submitEditBtn = document.querySelector('[name="submitEdit"]');
+    const textarea = document.getElementById('comment');
 
-        editBtn.classList.add('hidden');
-        cancelBtn.classList.remove('hidden');
-        submitEditBtn.classList.remove('hidden');
-        textarea.disabled = false;
-        document.querySelectorAll('input[name="rate"]').forEach(radio => radio.disabled = false);
-    }
+    editBtn.classList.add('hidden');
+    cancelBtn.classList.remove('hidden');
+    submitEditBtn.classList.remove('hidden');
+    textarea.disabled = false;
+    document.querySelectorAll('input[name="rate"]').forEach(radio => radio.disabled = false);
+}
 
-    function handleCancel() {
-        const editBtn = document.querySelector('[name="edit"]');
-        const cancelBtn = document.querySelector('[name="cancel"]');
-        const submitEditBtn = document.querySelector('[name="submitEdit"]');
-        const textarea = document.getElementById('comment');
+function handleCancel() {
+    const editBtn = document.querySelector('[name="edit"]');
+    const cancelBtn = document.querySelector('[name="cancel"]');
+    const submitEditBtn = document.querySelector('[name="submitEdit"]');
+    const textarea = document.getElementById('comment');
 
-        editBtn.classList.remove('hidden');
-        cancelBtn.classList.add('hidden');
-        submitEditBtn.classList.add('hidden');
-        textarea.disabled = true;
-        document.querySelectorAll('input[name="rate"]').forEach(radio => radio.disabled = true);
-    }
+    editBtn.classList.remove('hidden');
+    cancelBtn.classList.add('hidden');
+    submitEditBtn.classList.add('hidden');
+    textarea.disabled = true;
+    document.querySelectorAll('input[name="rate"]').forEach(radio => radio.disabled = true);
+}
 </script>
 
 
--- hospital_list.php
+++ hospital_list.php
@@ -13,21 +13,22 @@ $isHospitalExists = mysqli_num_rows($result) > 0;
 ?>
 
 <section class="content flex flex-col items-center pt-28 px-5 sm:px-20 -z-10 ">
-    <form class="flex items-center w-full border-2 rounded-lg">
+    <form class="flex items-center w-full w-1/2 md:w-3/4 xl:w-1/2 border-2 rounded-lg">
         <div class="relative w-full">
             <span class="absolute inset-y-0 left-0 flex items-center pl-3">
                 <i class="fa fa-search"></i>
             </span>
-            <input class="h-10 w-full pl-10 pr-3 text-center outline-none rounded-lg" type="text" name="query" placeholder="Cari rumah sakit berdasarkan nama, alamat..." value="<?= $query ?>">
+            <input class="h-10 w-full pl-10 pr-3 text-center outline-none rounded-lg" type="text" name="query"
+                placeholder="Cari rumah sakit berdasarkan nama, alamat..." value="<?= $query ?>">
             <?php if ($query) : ?>
-                <span class="absolute inset-y-0 right-0 flex items-center pr-3">
-                    <a href="hospital_list.php">
-                        <i class="fa fa-times"></i>
-                    </a>
-                </span>
-                <form method="post">
+            <span class="absolute inset-y-0 right-0 flex items-center pr-3">
+                <a href="hospital_list.php">
+                    <i class="fa fa-times"></i>
+                </a>
+            </span>
+            <form method="post">
 
-                </form>
+            </form>
             <?php endif; ?>
         </div>
         <button type="submit" style="display: none;"></button>
@@ -35,33 +36,35 @@ $isHospitalExists = mysqli_num_rows($result) > 0;
 
     <section class="flex">
         <?php if (!$isHospitalExists) : ?>
-            <div class="font-bold mt-10">Rumah sakit tidak ditemukan.</div>
+        <div class="font-bold mt-10">Rumah sakit tidak ditemukan.</div>
         <?php else : ?>
-            <div class="hospital_list grid grid-flow-row grid-cols-1  lg:grid-cols-2 xl:grid-cols-3 gap-5 my-10">
-                <?php while ($row = $result->fetch_assoc()) : ?>
-                    <div class="hospital_card flex flex-col justify-between border-2 border-gray-200 rounded-lg items-start bg-white p-3 sm:p-5">
-                        <div class="flex flex-col gap-5 items-start">
-                            <div class="title flex justify-center items-center gap-3">
-                                <div class="rating flex justify-center gap-2 items-center px-3 py-1 bg-[#DCE4FA] rounded-md">
-                                    <i class="fa fa-star text-yellow-500"></i>
-                                    <div class="font-bold"><?= $row['rating'] ?></div>
-                                </div>
-                                <span class="font-bold"><?= $row['name'] ?></span>
-                            </div>
-                            <div class="address"><?= $row['address'] ?></div>
+        <div class="hospital_list grid grid-flow-row grid-cols-1  lg:grid-cols-2 xl:grid-cols-3 gap-5 my-10">
+            <?php while ($row = $result->fetch_assoc()) : ?>
+            <div
+                class="hospital_card sm:w-[25em] flex flex-col justify-between border-2 border-gray-200 rounded-[10px] items-start bg-white p-3 sm:p-5">
+                <div class="flex flex-col gap-5 items-start">
+                    <div class="title flex justify-center items-center gap-3">
+                        <div class="rating flex justify-center gap-2 items-center px-3 py-1 bg-[#DCE4FA] rounded-md">
+                            <i class="fa fa-star text-yellow-500"></i>
+                            <div class="font-bold"><?= $row['rating'] ?></div>
                         </div>
-                        <div class="flex w-full justify-between mt-3">
-                            <div class="phone flex items-center gap-3">
-                                <i class="fas fa-phone p-2 bg-[#DCE4FA] rounded-md"></i>
-                                <span><?= $row['phone'] ?></span>
-                            </div>
-                            <a href="detail_hospital.php?hospital_id=<?= $row['hospital_id'] ?>" class="px-5 py-2 border-2 border-[#DCE4FA] rounded-full">
-                                Lihat Detail
-                            </a>
-                        </div>
+                        <span class="font-bold"><?= $row['name'] ?></span>
                     </div>
-                <?php endwhile; ?>
+                    <div class="address"><?= $row['address'] ?></div>
+                </div>
+                <div class="flex w-full justify-between mt-3">
+                    <div class="phone flex items-center gap-3">
+                        <i class="fas fa-phone p-2 bg-[#DCE4FA] rounded-md"></i>
+                        <span><?= $row['phone'] ?></span>
+                    </div>
+                    <a href="detail_hospital.php?hospital_id=<?= $row['hospital_id'] ?>"
+                        class="px-5 py-2 border-2 border-[#DCE4FA] rounded-full">
+                        Lihat Detail
+                    </a>
+                </div>
             </div>
+            <?php endwhile; ?>
+        </div>
 
         <?php endif; ?>
     </section>
